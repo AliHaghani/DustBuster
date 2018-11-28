ed8a5c507cd41acdf02377d58da246edd311831b
Ali Haghani <alihaghani96@gmail.com>
2018-11-27 16:03:47 -0800

    Initial commit.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app     = express();
+const util    = require('./util');
+
+app.get('/bust', async (req, res) => {
+  let projectUrl = req.param('url');
+  res.send('Ok sick, got URL.');
+});
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.use('/assets', express.static('assets'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+exports = module.exports = app;
---
+++ util.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getProject: async (projectUrl) => {
+
+  },
+};
